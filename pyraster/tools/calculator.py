d722c57c4b4134f662f35b82f3b6e870979257f2
benjamin <benjamin.pillot@ird.fr>
2020-09-24 13:20:48 +0200

    Modify "_raster_calculation" function
    * The function might now be used for one or several raster, for any number of bands. The only condition is to return a raster

--- pyraster/tools/calculator.py
+++ pyraster/tools/calculator.py
@@ -9,6 +9,8 @@ __author__ = 'Benjamin Pillot'
 __copyright__ = 'Copyright 2020, Benjamin Pillot'
 __email__ = 'benjaminpillot@riseup.net'
 
+import numpy as np
+
 from pyraster import FLOAT32
 from pyraster.io import RasterTempFile
 from pyraster.tools import _gdal_temp_dataset, _return_raster
@@ -47,29 +49,40 @@ def _op(raster1, out_file, raster2, op_type):
     out_ds = None
 
 
-def _raster_calculation(sources, fhandle, window_size, gdal_driver):
+def _raster_calculation(raster_class, sources, fhandle, window_size, gdal_driver, data_type, **kwargs):
     """ Calculate raster expression
 
     """
     master_raster = sources[0]
     with RasterTempFile(gdal_driver.GetMetadata()['DMD_EXTENSION']) as out_file:
 
-        out_ds = _gdal_temp_dataset(out_file.path, gdal_driver, master_raster._gdal_dataset.GetProjection(),
-                                    master_raster.x_size, master_raster.y_size, master_raster.nb_band,
-                                    master_raster.geo_transform, FLOAT32, master_raster.no_data)
-        length = (master_raster.x_size//window_size + 1) * \
-                 (master_raster.y_size//window_size + 1) * master_raster.nb_band
+        length = (master_raster.x_size//window_size + int(master_raster.x_size % window_size != 0)) * \
+                 (master_raster.y_size//window_size + int(master_raster.y_size % window_size != 0))
+
+        is_first_run = True
 
         for window in tqdm(get_block_windows(window_size, master_raster.x_size, master_raster.y_size),
                            total=length, desc="Calculate raster expression"):
-            for band in range(1, master_raster.nb_band + 1):
-                list_of_arrays = []
-                for src in sources:
-                    list_of_arrays.append(src._gdal_dataset.GetRasterBand(band).ReadAsArray(*window).astype("float32"))
+            list_of_arrays = []
+            for src in sources:
+                list_of_arrays.append(src._gdal_dataset.ReadAsArray(*window).astype("float32"))
 
-                out_ds.GetRasterBand(band).WriteArray(fhandle(*list_of_arrays), window[0], window[1])
+            result = fhandle(*list_of_arrays, **kwargs)
+
+            if is_first_run:
+                if result.ndim == 2:
+                    result = np.expand_dims(result, axis=0)
+
+                out_ds = _gdal_temp_dataset(out_file.path, gdal_driver, master_raster._gdal_dataset.GetProjection(),
+                                            master_raster.x_size, master_raster.y_size, result.shape[0],
+                                            master_raster.geo_transform, data_type, master_raster.no_data)
+
+                is_first_run = False
+
+            for band in range(result.shape[0]):
+                out_ds.GetRasterBand(band + 1).WriteArray(result[band, :, :], window[0], window[1])
 
     # Close dataset
     out_ds = None
 
-    return master_raster.__class__(out_file.path)
+    return raster_class(out_file.path)
