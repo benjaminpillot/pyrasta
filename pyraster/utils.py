9a2ae1669990c08dd875c3410e03d89cb9ed0166
benjamin <benjamin.pillot@ird.fr>
2020-11-21 22:40:51 +0100

    Add homemade "digitize" function (no numpy) and progress bar for url download

--- pyraster/utils.py
+++ pyraster/utils.py
@@ -7,6 +7,34 @@ More detailed description.
 from collections import Collection
 from itertools import chain, islice
 
+from tqdm import tqdm
+
+
+class TqdmUpTo(tqdm):
+    """ Progress bar for url retrieving
+
+    Description
+    -----------
+    Thanks to https://github.com/tqdm/tqdm/blob/master/examples/tqdm_wget.py
+
+    Provides `update_to(n)` which uses `tqdm.update(delta_n)`.
+    Inspired by [twine#242](https://github.com/pypa/twine/pull/242),
+    [here](https://github.com/pypa/twine/commit/42e55e06).
+    """
+
+    def update_to(self, b=1, bsize=1, tsize=None):
+        """
+        b  : int, optional
+            Number of blocks transferred so far [default: 1].
+        bsize  : int, optional
+            Size of each block (in tqdm units) [default: 1].
+        tsize  : int, optional
+            Total size (in tqdm units). If [default: None] remains unchanged.
+        """
+        if tsize is not None:
+            self.total = tsize
+        return self.update(b * bsize - self.n)  # also sets self.n = b * bsize
+
 
 def check_string(string, list_of_strings):
     """ Check validity of and return string against list of valid strings
@@ -43,6 +71,24 @@ def check_type(*args):
             if not isinstance(args[item], args[item + 1]):
                 raise TypeError("Type of argument {} is '{}' but must be '{}'".format(
                     item//2 + 1, type(args[item]).__name__, args[item + 1].__name__))
+
+
+def digitize(value, list_of_values, ascend=True, right=False):
+    """
+
+    Description
+    -----------
+
+    Parameters
+    ----------
+
+    """
+    if ascend:
+        loc = [value > v if right else value >= v for v in list_of_values]
+    else:
+        loc = [value <= v if right else value < v for v in list_of_values]
+
+    return loc.index(False) if False in loc else len(list_of_values)
 
 
 def lazyproperty(func):
