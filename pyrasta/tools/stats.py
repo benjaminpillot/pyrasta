b9b44cf694f33eb074ba6871efbeba6f801f02c2
benjaminpillot <benjaminpillot@riseup.net>
2021-03-11 11:38:23 +0100

    Fix line breaks

--- pyrasta/tools/stats.py
+++ pyrasta/tools/stats.py
@@ -17,9 +17,10 @@ def _histogram(raster, nb_bins, normalized):
     for band in range(raster.nb_band):
         edges = np.linspace(raster.min, raster.max, nb_bins + 1)
         hist_x = edges[0:-1] + (edges[1::] - edges[0:-1])/2
-        hist_y = np.asarray(raster._gdal_dataset.GetRasterBand(band + 1).GetHistogram(min=raster.min[band],
-                                                                                      max=raster.max[band],
-                                                                                      buckets=nb_bins))
+        hist_y = np.asarray(
+            raster._gdal_dataset.GetRasterBand(band + 1).GetHistogram(min=raster.min[band],
+                                                                      max=raster.max[band],
+                                                                      buckets=nb_bins))
         if normalized:
             hist_y = hist_y / np.sum(hist_y)
 
