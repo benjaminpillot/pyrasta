de544196cdda8a84854082e8b640a6f7dcdac34d
benjaminpillot <benjaminpillot@riseup.net>
2021-03-14 22:08:00 +0100

    Update Clipping functions

--- pyrasta/tools/clip.py
+++ pyrasta/tools/clip.py
@@ -12,7 +12,7 @@ import gdal
 
 
 @_return_raster
-def _clip_raster_by_extent(raster, out_file, bounds):
+def _clip_raster_by_extent(raster, out_file, bounds, no_data):
     """ Clip raster by extent
 
     Parameters
@@ -21,6 +21,8 @@ def _clip_raster_by_extent(raster, out_file, bounds):
     out_file: pyrasta.io_.files.RasterTempFile
     bounds: tuple
         boundaries as (minx, miny, maxx, maxy)
+    no_data: int or float
+        No data value
 
     Returns
     -------
@@ -39,20 +41,20 @@ def _clip_raster_by_extent(raster, out_file, bounds):
               raster._gdal_dataset,
               outputBounds=bounds,
               srcNodata=raster.no_data,
-              dstNodata=raster.no_data,
+              dstNodata=no_data,
               outputType=raster.data_type)
 
 
-def _clip_raster_by_feature(raster, geodataframe, id_feature, all_touched):
-    """ Clip raster by mask from feature in layer
+def _clip_raster_by_mask(raster, geodataframe, no_data, all_touched):
+    """ Clip raster by mask from geographic layer
 
     Parameters
     ----------
     raster: pyrasta.raster.RasterBase
         raster to clip
-    geodataframe: geopandas.GeoDataFrame
-    id_feature: int
-        feature ID
+    geodataframe: geopandas.GeoDataFrame or gistools.layer.GeoLayer
+    no_data: float or int
+        No data value
     all_touched: bool
         if True, clip all pixels that are touched, otherwise clip
         if pixel's centroids are within boundaries
@@ -61,14 +63,12 @@ def _clip_raster_by_feature(raster, geodataframe, id_feature, all_touched):
     -------
 
     """
-
-    feature = geodataframe.iloc[[id_feature]].to_crs(raster.crs)
-    clip_raster = raster.clip(bounds=feature.bounds.iloc[id_feature].values)
+    clip_raster = raster.clip(bounds=geodataframe.total_bounds)
 
     with ShapeTempFile() as shp_file, \
             RasterTempFile(clip_raster._gdal_driver.GetMetadata()['DMD_EXTENSION']) as r_file:
 
-        feature.to_file(shp_file.path, driver=ESRI_DRIVER)
+        geodataframe.to_file(shp_file.path, driver=ESRI_DRIVER)
 
         out_ds = _gdal_temp_dataset(r_file.path,
                                     clip_raster._gdal_driver,
@@ -90,4 +90,5 @@ def _clip_raster_by_feature(raster, geodataframe, id_feature, all_touched):
     return clip_raster.__class__.raster_calculation([clip_raster,
                                                      clip_raster.__class__(r_file.path)],
                                                     lambda x, y: x*y,
+                                                    no_data=no_data,
                                                     showprogressbar=False)
