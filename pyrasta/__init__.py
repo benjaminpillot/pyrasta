4b80fd392885b726fc7b3c16be26af583e78c6b7
benjaminpillot <benjaminpillot@riseup.net>
2021-03-15 14:37:24 +0100

    New version

--- pyrasta/__init__.py
+++ pyrasta/__init__.py
@@ -5,7 +5,7 @@
 More detailed description.
 """
 
-__version__ = '1.2'
+__version__ = '1.2.1'
 __author__ = 'Benjamin Pillot'
 __copyright__ = 'Copyright 2020, Benjamin Pillot'
 __email__ = 'benjaminpillot@riseup.net'
