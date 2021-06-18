e351a88aa21e8f1b0212b981edfdc954f88ee859
benjaminpillot <benjaminpillot@riseup.net>
2021-06-18 21:19:08 +0200

    Add K-MEANS classification functions

--- pyrasta/algorithms/classification.py
+++ pyrasta/algorithms/classification.py
@@ -15,6 +15,9 @@ from sklearn.cluster import KMeans
 import gdal
 
 
+CLASSIFICATION_NO_DATA = -1
+
+
 def return_classification(classification):
     @wraps(classification)
     def _return_classification(raster, nb_classes, *args, **kwargs):
@@ -23,7 +26,7 @@ def return_classification(classification):
                                         raster._gdal_dataset.GetProjection(),
                                         raster.x_size, raster.y_size, 1,
                                         raster.geo_transform, gdal.GetDataTypeByName('Int16'),
-                                        no_data=-999)
+                                        no_data=CLASSIFICATION_NO_DATA)
             labels = classification(raster, nb_classes, out_ds, *args, **kwargs)
 
             # Close dataset
@@ -37,17 +40,41 @@ def return_classification(classification):
 
 
 @return_classification
-def _k_means_classification(raster, nb_clusters, out_ds, n_init, max_iter):
+def _k_means_classification(raster, nb_clusters, out_ds, *args, **kwargs):
     """ Apply k-means classification
 
     """
-    k_means_classifier = KMeans(nb_clusters, n_init=n_init, max_iter=max_iter)
-    values = np.reshape(raster._gdal_dataset.ReadAsArray(),
-                        (raster.nb_band, raster.x_size * raster.y_size)).transpose()
-    labels = np.reshape(k_means_classifier.fit(values).labels_, (raster.y_size, raster.x_size))
+    k_means_classifier = KMeans(nb_clusters, *args, **kwargs)
+    samples = np.reshape(raster._gdal_dataset.ReadAsArray(),
+                         (raster.nb_band, raster.x_size * raster.y_size)).transpose()
+    labels = k_means_classifier.fit(samples).labels_
+    labels = np.reshape(labels, (raster.y_size, raster.x_size))
 
     out_ds.GetRasterBand(1).WriteArray(labels)
 
 
-def k_means_classification(raster, nb_clusters, n_init=100, max_iter=1000):
-    _k_means_classification(raster, nb_clusters, n_init, max_iter)
+def k_means(raster, nb_clusters, *args, **kwargs):
+    """ Compute k-means on given raster
+
+    Description
+    -----------
+    Run k-means algorithm on raster data
+
+    Warning
+    -------
+    As the algorithm requires to process all data, all values
+    are written to memory. Be sure your machine has enough memory to run it.
+
+    Parameters
+    ----------
+    raster
+    nb_clusters
+    args, kwargs :
+        sklearn.cluster.KMeans arguments and keyword arguments
+        (see scikit-learn documentation)
+
+    Returns
+    -------
+
+    """
+    _k_means_classification(raster, nb_clusters, *args, **kwargs)
